1b86fa589d0a941ad9ede126804792b996f40a42
Christopher Doohan <chrisdoohan@gmail.com>
2026-02-14 23:56:01 -0800

    Implement bayer dithering

--- app/assets/javascripts/image_editor.js
+++ app/assets/javascripts/image_editor.js
@@ -287,8 +287,12 @@
       output = thresholdConvert(pixels, 128);
     } else if (ditherMode === 'floyd-steinberg') {
       output = floydSteinberg(pixels, w, h);
-    } else {
+    } else if (ditherMode === 'atkinson') {
       output = atkinsonDither(pixels, w, h);
+    } else if (ditherMode === 'bayer4') {
+      output = bayerDither(pixels, w, h, BAYER4, 4);
+    } else {
+      output = bayerDither(pixels, w, h, BAYER8, 8);
     }
 
     // 6. Render
@@ -336,6 +340,37 @@
         if (y + 1 < h)               pixels[idx + w]     += err;
         if (y + 1 < h && x + 1 < w)  pixels[idx + w + 1] += err;
         if (y + 2 < h)               pixels[idx + 2 * w] += err;
+      }
+    }
+    return out;
+  }
+
+  // Bayer ordered dithering matrices (pre-normalized to 0-1 thresholds)
+  var BAYER4 = [
+     0/16,  8/16,  2/16, 10/16,
+    12/16,  4/16, 14/16,  6/16,
+     3/16, 11/16,  1/16,  9/16,
+    15/16,  7/16, 13/16,  5/16
+  ];
+
+  var BAYER8 = [
+     0/64, 32/64,  8/64, 40/64,  2/64, 34/64, 10/64, 42/64,
+    48/64, 16/64, 56/64, 24/64, 50/64, 18/64, 58/64, 26/64,
+    12/64, 44/64,  4/64, 36/64, 14/64, 46/64,  6/64, 38/64,
+    60/64, 28/64, 52/64, 20/64, 62/64, 30/64, 54/64, 22/64,
+     3/64, 35/64, 11/64, 43/64,  1/64, 33/64,  9/64, 41/64,
+    51/64, 19/64, 59/64, 27/64, 49/64, 17/64, 57/64, 25/64,
+    15/64, 47/64,  7/64, 39/64, 13/64, 45/64,  5/64, 37/64,
+    63/64, 31/64, 55/64, 23/64, 61/64, 29/64, 53/64, 21/64
+  ];
+
+  function bayerDither(pixels, w, h, matrix, n) {
+    var out = new Uint8Array(pixels.length);
+    for (var y = 0; y < h; y++) {
+      for (var x = 0; x < w; x++) {
+        var idx = y * w + x;
+        var threshold = matrix[(y % n) * n + (x % n)] * 255;
+        out[idx] = pixels[idx] > threshold ? 255 : 0;
       }
     }
     return out;
